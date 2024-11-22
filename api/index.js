2fd7e180120ce2b45697493f6d4fedface03f182
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-11-22 14:41:34 +0530

    Max timeout 25s

--- api/index.js
+++ api/index.js
@@ -30,13 +30,13 @@ app.post('/', async (req, res) => {
 
     try {
         // Check if user already exists by phone
-        const existingUser = await UserModel.findOne({ phone });
+        const existingUser = await UserModel.findOne({ phone }).maxTimeMS(25000);
         if (existingUser) {
             return res.redirect("https://onlinemanipalmba.in/");
         }
 
         // Create and save new user
-        const newUser = new UserModel({ name, email, phone, course });
+        const newUser = new UserModel({ name, email, phone, course }).maxTimeMS(25000);
         await newUser.save();
 
         res.redirect("https://onlinemanipalmba.in/");
